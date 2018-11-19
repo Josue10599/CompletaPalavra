37bfc1ef13e63f1f8b979785d0e7bd8635ee3fdb
Josue Lopes <josue10599@gmail.com>
2018-11-19 21:24:43 -0200

    Adição da classe Palavra

---
+++ CompletaNome/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompletaNome
+{
+    public partial class Form1 : Form
+    {
+        Button[] button;
+        Palavras palavra;
+
+        public Form1()
+        {
+            InitializeComponent();
+            palavra = new Palavras();
+            criaBotoes();
+            label1.Text = palavra.palavraIncompleta();
+            label2.Text = palavra.getPalavra();
+        }
+
+        private void criaBotoes()
+        {
+            button = new Button[palavra.numCaracter()];
+            int i = 0, horizontal = 12;
+            foreach (var item in palavra.retornaCaracter())
+            {
+                button[i] = new Button();
+                button[i].AutoSize = true;
+                button[i].Location = new System.Drawing.Point(horizontal, 266);            
+                button[i].Size = new System.Drawing.Size(75, 23);
+                button[i].Name = "button" + i;
+                button[i].TabIndex = i;
+                button[i].Text = item.ToString();
+                button[i].UseVisualStyleBackColor = true;
+                button[i].Click += new System.EventHandler(this.button_Click);
+                button[i].Visible = true;
+                button[i].Enabled = true;
+                this.Controls.Add(button[i]);
+                horizontal += 81;
+                i++;
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (((Button)sender).Text.Equals(palavra.getCaracterRemovido().ToString()))
+                MessageBox.Show("Parabéns");
+        }
+    }
+}
---
+++ CompletaNome/Palavras.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompletaNome
+{
+    class Palavras
+    {
+        private int idPalavra;
+        private string palavra;
+        private char caracterRemovido;
+
+        public Palavras()
+        {
+            palavra = "vermelho";
+            palavra = palavra.ToUpper();
+            this.caracterRemovido = palavra[new Random().Next(palavra.Length)];
+        }
+        
+        public string getPalavra()
+        {
+            return palavra;
+        }
+
+        public char getCaracterRemovido()
+        {
+            return this.caracterRemovido;
+        }
+
+        public int numCaracter()
+        {
+            return getPalavra().Length;
+        }
+
+        public string retornaCaracter(int posicao)
+        {
+            return palavra[posicao].ToString();
+        }
+
+        public ArrayList retornaCaracter()
+        {
+            ArrayList caracter = new ArrayList();
+            for(int i = 0; i < palavra.Length; i++)
+            {
+                if (!caracter.Contains(palavra[i]))
+                    caracter.Add(palavra[i]);
+            }
+            return caracter;
+        }
+
+        public string palavraIncompleta()
+        {
+            return palavra.Replace(getCaracterRemovido(), '_');
+        }
+    }
+}
